3a027b8446ed0433923a6112190ea0f8689ef9c6
Esthuardo Quispe <quispefesthuardo@gmail.com>
2023-11-19 22:39:41 -0500

    correcion intento 2 render

--- TiendaProyectoFinal/settings.py
+++ TiendaProyectoFinal/settings.py
@@ -64,7 +64,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware"  # RENDER MIDDLEWARE
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # RENDER MIDDLEWARE
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
